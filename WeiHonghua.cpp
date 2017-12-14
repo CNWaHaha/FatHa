86cfe9ed6c7840c5f4115c04a1f8b6453f360539
CNWaHaha <34511414+CNWaHaha@users.noreply.github.com>
2017-12-14 22:00:05 +0800

    Add files via upload

---
+++ WeiHonghua.cpp
@@ -0,0 +1,7 @@
+for( int n = 0; n < rowofClass ; n++) //从二维数组Class的n+1行开始
+   {
+   	   for( int m = 0; m < columnofClass; m++) //从二维数组Class的m+1列开始
+	   {
+
+	   }
+   } //Class[n][m]就表示数组的第(n+1)*(m+1)个元素
